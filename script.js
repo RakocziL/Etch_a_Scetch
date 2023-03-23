3f60be254dc8c64741a28d666e266b288017f150
RakocziL <levente.rakoczi@gmail.com>
2023-03-23 12:53:36 +0100

    I keep forgeting to make more comits

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-//Select the main container
+//Select elements
 const container = document.querySelector(".container");
 const slider = document.querySelector("#gridSlider");
+const buttonBlack =document.querySelector("#black");
+const buttonColorChange = document.querySelector("#colorChange");
+const buttonClear = document.querySelector("#clear");
+const colorPicker = document.querySelector("#colorPicker");
 
+//Addin event listeners
 slider.addEventListener("change", gridRange );
+buttonBlack.addEventListener("click", changeToBlack);
+buttonColorChange.addEventListener("click", changeToColorChange);
+buttonClear.addEventListener("click", gridClear);
+colorPicker.addEventListener("change", changeColorPicker);
+colorPicker.addEventListener("click", changeColorPicker);
+
 
 //test function
 function test(e){
-    console.log("Jeno");
+    console.log("Jenő");
 }
 
 //Grid creator function
@@ -21,10 +32,6 @@ function createGrid(height){
             const cell = document.createElement("div");
             cell.classList.add("grid-cell");
             //Mouseover colorchange
-            cell.addEventListener("mouseover", function(){
-                const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-                cell.style.backgroundColor = color;
-            });
             row.appendChild(cell);
         }
     }
@@ -46,9 +53,48 @@ function gridRange(e){
 }
 
 //Swap between colors
-function colorChanger(){
-    
+function colorChanger(color){
+    const cells = document.querySelectorAll(".grid-cell");
+    if(color=="CHANGE"){
+        for(var i = 0; i < cells.length;i++){
+            const cell = cells[i];
+            cell.addEventListener("mouseover", function(){
+            color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            cell.style.backgroundColor = color;
+            })
+            }
+    }
+
+    else if(color=="white"){
+        for(var i = 0; i < cells.length;i++){
+            const cell = cells[i];
+            cell.style.backgroundColor = color;
+            }
+    }
+    else{
+        for(var i = 0; i < cells.length;i++){
+            const cell = cells[i];
+            cell.addEventListener("mouseover", function(){
+            cell.style.backgroundColor = color;
+            })
+        }
+    }
 }
+    //Colors
+    function changeToBlack(){
+        colorChanger("black");
+    }
+    function changeToColorChange(){
+        colorChanger("CHANGE");
+    }
+    function changeColorPicker(){
+        colorChanger(colorPicker.value);
+    }
+    //Clear the whole grid
+    function gridClear(){
+        colorChanger("white");
+    }
+
 
 
 gridRange()
